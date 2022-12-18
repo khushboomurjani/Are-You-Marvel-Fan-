da208dddcb6c9e8110c6000861b6a7327fb9c6ab
khushboo Murjani <42289127+khushboomurjani@users.noreply.github.com>
2022-12-18 08:29:33 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,72 @@
+var readlineSync = require("readline-sync");
+
+var score = 0;
+
+var highScores = [
+  {
+    name: "Khushboo",
+    score: 3,
+  },
+
+  {
+    name: "Shiva",
+    score: 2,
+  },
+]
+
+// array of objects
+var questions = [{
+  question: "What's the original name of Captain America?",
+  answer: "Steve Rogers"
+}, {
+  question: "What's Thor's hammer called?",
+  answer: "Mjolnir"
+},
+{
+  question: "Do you know what's Spiderman's real name?",
+  answer: "Peter Parker"
+}];
+
+function welcome() {
+  var userName = readlineSync.question("What's your name? ");
+
+  console.log("Welcome " + userName + " let's see if you know Marvel Cinematic Universe");
+}
+
+
+// play function
+function play(question, answer) {
+  var userAnswer = readlineSync.question(question);
+
+  if (userAnswer.toUpperCase() === answer.toUpperCase()) { // branching
+    console.log("right!");
+    score = score + 1;
+
+  } else {
+    console.log("wrong!");
+
+  }
+
+  console.log("current score: ", score);
+  console.log("-------------")
+}
+
+function game() {
+  for (var i = 0; i < questions.length; i++) {
+    var currentQuestion = questions[i];
+    play(currentQuestion.question, currentQuestion.answer)
+  }
+}
+
+function showScores() {
+  console.log("YAY! You SCORED: ", score);
+
+  console.log("Check out the high scores, if you should be there ping me and I'll update it");
+
+  highScores.map(score => console.log(score.name, " : ", score.score))
+}
+
+
+welcome();
+game();
+showScores();
